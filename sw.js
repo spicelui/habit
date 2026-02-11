af20fffdad31d582671a177430e92998a5906672
spicelui <spicelui@gmail.com>
2026-02-10 20:52:50 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'habitflow-v1.3.5'; // cambia la versión cada vez que actualices archivos
+const CACHE_NAME = 'habitflow-v1.2'; // cambia la versión cada vez que actualices archivos
 const ASSETS = [
   './',
   './index.html',
@@ -50,4 +50,3 @@ self.addEventListener('fetch', e => {
     })
   );
 });
-
